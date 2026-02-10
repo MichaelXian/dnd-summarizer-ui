7f9f1e3578f3e6ef5358637587597d794b155cf5
Michael Xian <michaelkxian@gmail.com>
2026-02-09 19:55:25 -0800

    initial FastAPI setup

---
+++ server/src/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:3000",  # React dev server
+    "http://127.0.0.1:3000"   # sometimes React uses this instead
+]
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
